b61b3a6c6dd1f962f1d61afbb24b27883e30700d
Soldatova-Kristina <soldatovadev98@icloud.com>
2025-07-15 15:37:43 -0300

    fix: align input caret with placeholder text

---
+++ js/logic.js
@@ -0,0 +1,14 @@
+import { createLayout } from "./layout";
+const {
+    addTaskInput,
+    addTaskBtn,
+    allTasksCountBadge,
+    doneTasksCountBadge,
+    emptyStorage,
+    form,
+    storageContainer
+  } = createLayout();
+  
+addTaskInput.addEventListener("submit", (e) => {
+    
+}) 
